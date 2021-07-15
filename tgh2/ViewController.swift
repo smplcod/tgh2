da68cdd9e96871ce1c16234110301ea792b447db
smplcod <smplcod2021@gmail.com>
2021-07-15 18:24:02 +0400

    Initial Commit

---
+++ tgh2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tgh2
+//
+//  Created by smplcod on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
